042fd166b7a74cd122376e7cdeaa71bb20daea5c
Dara Harsha Vardhini <160300053+vardhini8242@users.noreply.github.com>
2026-05-18 17:19:54 +0530

    kpm_definesh.h

---
+++ kpm_definesh.h
@@ -0,0 +1,12 @@
+#define ROW_0 16 //p1.16
+#define ROW_1 17 //p1.17
+#define ROW_2 18 //p1.18
+#define ROW_3 19 //p1.19
+
+#define cOL_0 20 //p1.20
+#define COL_1 21 //p1.21
+#define COL_2 22 //p1.22
+#define COL_3 23  //p1.23
+
+#define ROWs_4 16 //p1.16
+#define COLs_4 20 //p1.20
