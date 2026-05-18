838bf33887ef5fd0b9a7bf096420a4aa7bef292d
Dara Harsha Vardhini <160300053+vardhini8242@users.noreply.github.com>
2026-05-18 17:08:04 +0530

    delay.h

---
+++ delay.h
@@ -0,0 +1,10 @@
+                         /* delay.h */
+#ifndef __DELAY_H__
+#define __DELAY_H__
+
+#include "types.h"
+void delay_ms(u32 dlyMS);
+void delay_us(u32 dlyUS);
+void delay_s(u32 dlyS);
+
+#endif
